b9c93f07ad1cea9183f9a14407432cdff347582d
PeteNelson22 <peternelson063@gmail.com>
2023-01-22 01:26:38 +0100

    Added discovery server

--- order-service/src/main/java/com/nelson/orderservice/Service/OrderService.java
+++ order-service/src/main/java/com/nelson/orderservice/Service/OrderService.java
@@ -22,7 +22,7 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
-    private final WebClient webClient;
+    private final WebClient.Builder webClient;
 
     public void placeOrder(OrderRequest orderRequest){
         Order order = new Order();
@@ -39,8 +39,8 @@ public class OrderService {
                 .map(OrderLineItem::getSkuCode)
                 .toList();
 
-        InventoryResponse[] inventoryResponses = webClient.get()
-                .uri("http://localhost:8082/api/inventory",
+        InventoryResponse[] inventoryResponses = webClient.build().get()
+                .uri("http://inventory-service/api/inventory",
                         uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
                 .retrieve()
                 .bodyToMono(InventoryResponse[].class)
