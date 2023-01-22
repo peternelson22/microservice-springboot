a9da86b7dd7c3f81c7b255d4afb9909c1dfd95df
PeteNelson22 <peternelson063@gmail.com>
2023-01-22 11:23:40 +0100

    Added keycloak authentication

---
+++ api-gateway/src/main/java/com/nelson/apigateway/Config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.nelson.apigateway.Config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    public SecurityWebFilterChain filterChain(ServerHttpSecurity http){
+        http
+                .csrf().disable()
+                .authorizeExchange(exchange -> exchange
+                        .pathMatchers("*/eureka/**")
+                        .permitAll()
+                        .anyExchange()
+                        .authenticated())
+                .oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt);
+        return http.build();
+    }
+}
